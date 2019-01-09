7e9367a37fcc470e924bdc5be5f9b7739fd476e4
Julian Barnes <julianbarnes@Julians-MacBook-Pro.local>
2019-01-09 12:11:22 -0500

    remove aws authentication

--- src/index.js
+++ src/index.js
@@ -2,17 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import awsMobile from './aws-exports';
-import Amplify from 'aws-amplify';
-import { withAuthenticator  } from "aws-amplify-react";
-import registerServiceWorker from './serviceWorker';
-
-
-Amplify.configure(awsMobile);
-const AppWithAuth = withAuthenticator(App)
-ReactDOM.render(<AppWithAuth />, document.getElementById('root'));
 
 
 
-serviceWorker.unregister();
+ReactDOM.render(<App/>, document.getElementById('root'));
+
+
