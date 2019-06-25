41bca9ca808e4d230011fa0334c7d891fbf82f4e
Julian Barnes <julianbarnes@Julians-MacBook-Pro.local>
2019-06-25 11:04:47 -0400

    Freezing up

---
+++ backend/models/event.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const eventSchema = new Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    date: { type: Date, required: true }
+  }, {
+    timestamps: true,
+  });
+
+const Event = mongoose.model('Event', eventSchema);
+
+module.exports = Event;
---
+++ backend/routes/events.js
@@ -0,0 +1,58 @@
+const router = require('express').Router();
+let Event = require('../models/event.model');
+/**
+ * Lists all of the events currently in the database
+ */
+router.route('/').get((req, res) => {
+  console.log("It's hitting the breakpoint")
+  Event.find()
+    .then(events => res.json(events))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+/**
+ * Adds an event
+ * Inputs: Title, description, and date
+ */
+router.route('/add').post((req, res) => {
+  const title = req.body.title;
+  const description = req.body.description;
+  const date = Date.parse(req.body.date);
+
+  const newEvent = new Event({
+    title,
+    description,
+    date
+  });
+
+  newEvent.save()
+  .then(() => res.json('Event added!'))
+  .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').get((req, res) => {
+  Event.findById(req.params.id)
+    .then(event => res.json(event))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+  Event.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Event deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+  Event.findById(req.params.id)
+    .then(event => {
+      event.username = req.body.username;
+      event.description = req.body.description;
+      event.date = Date.parse(req.body.date);
+
+      event.save()
+        .then(() => res.json('Event updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
